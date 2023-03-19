ace162f544c4988c42ddebdd7019dfc2ec0d7500
codefirst2022 <codefirst2022@gmail.com>
2023-03-19 15:38:44 +0530

    chnages

---
+++ menu.js
@@ -0,0 +1,56 @@
+var angularMenuArray = [
+    {
+        url:'../angular/ngIf.html',
+        text: 'ngIf'
+    },
+    {
+        url:'../angular/ngfor.html',
+        text: 'ngFor'
+    },
+    {
+        url:'../angular/ngclass.html',
+        text: 'ngClass'
+    } 
+];
+
+var logicalMenuArray = [
+    {
+        url:'../logical/broadband-plan.html',
+        text: 'Broadband-Plan'
+    },
+    {
+        url:'../logical/train-booking.html',
+        text: 'Train-Booking'
+    },
+    {
+        url:'../logical/movieBooking.html',
+        text: 'Movie-Booking'
+    } 
+];
+
+var movieArray = [
+    {
+        movieName: 'Rocketry: The Nambi Effect',
+        ticketRate: '250',
+        description: 'Based on the life of Indian Space Research Org scientist Nambi Narayanan, who was framed for being a spy ',
+        shows: ['09.00 AM - 12.00 AM', '12.30 PM - 1.30 PM', ' 1.30 PM - 3.30' ]
+    },
+    {
+        movieName: '3 Idiots',
+        ticketRate: '300',
+        description: 'Two friends are searching for their long lost companion. They revisit their college days and recall the memories.',
+        shows: ['10.00 AM - 12.00 PM', '03.00 PM - 6.00 PM']
+    },
+    {
+        movieName: 'Dangal',
+        ticketRate: '200',
+        description: 'wrestler Mahavir Singh Phogat and his two wrestler daughters struggle towards glory at the Commonwealth Games.',
+        shows: ['04.00 PM - 07.00 PM']
+    },
+    {
+        movieName: 'Drishyam 2',
+        ticketRate: '240',
+        description: 'an investigation and a family which is threatened by it. Will Georgekutty be able to protect his family this time?',
+        shows: ['11.00 AM - 02.00 PM', '02.00 PM - 05.00 PM']
+    }
+]
