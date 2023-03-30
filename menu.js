0aa0e2b9fa349331907ae8bfb811f2f47bf977e7
codefirst2022 <codefirst2022@gmail.com>
2023-03-30 09:21:33 +0530

    changes

--- menu.js
+++ menu.js
@@ -1,66 +1,66 @@
 var angularMenuArray = [
     {
-        url:'https://voidchetan.github.io/voidchetan/angular/ngIf.html',
+        url: 'https://voidchetan.github.io/voidchetan/angular/ngIf.html',
         text: 'ngIf',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://i.stack.imgur.com/IQIbb.png'
     },
     {
-        url:'https://voidchetan.github.io/voidchetan/angular/ngfor.html',
+        url: 'https://voidchetan.github.io/voidchetan/angular/ngfor.html',
         text: 'ngFor',
         info: 'multple wqays to uswe ngFor with examples',
-        imageUrl:'https://i.stack.imgur.com/CoXLh.png'
+        imageUrl: 'https://i.stack.imgur.com/CoXLh.png'
     },
     {
-        url:'https://voidchetan.github.io/voidchetan/angular/ngclass.html',
+        url: 'https://voidchetan.github.io/voidchetan/angular/ngclass.html',
         text: 'ngClass',
         info: 'how to add dynammic class  in angular check examples',
-        imageUrl:'https://daily-dev-tips.com/images/31-03-2021.jpg'
-    } ,
+        imageUrl: 'https://daily-dev-tips.com/images/31-03-2021.jpg'
+    },
     {
-        url:'https://voidchetan.github.io/voidchetan/angular/ngStyle.html',
+        url: 'https://voidchetan.github.io/voidchetan/angular/ngStyle.html',
         text: 'ngStyle',
         info: 'how to add dynamic css using ngStyle',
-        imageUrl:'https://dsinfosrl.files.wordpress.com/2019/07/image.png'
-    }  ,
+        imageUrl: 'https://dsinfosrl.files.wordpress.com/2019/07/image.png'
+    },
     {
-        url:'https://voidchetan.github.io/voidchetan/angular/forms.html',
+        url: 'https://voidchetan.github.io/voidchetan/angular/forms.html',
         text: 'Forms',
         info: 'how to add dynamic css using ngStyle',
-        imageUrl:'https://dsinfosrl.files.wordpress.com/2019/07/image.png'
-    } 
+        imageUrl: 'https://dsinfosrl.files.wordpress.com/2019/07/image.png'
+    }
 ];
 var studentArray = [
     {
-        name:'Sachin tendulkar',
+        name: 'Sachin tendulkar',
         age: 53,
         attendance: 50,
         gender: 'M',
         isIndian: true
     },
     {
-        name:'Rahul D',
+        name: 'Rahul D',
         age: 58,
         attendance: 67,
         gender: 'M',
         isIndian: true
     },
     {
-        name:'Jesika Harries',
+        name: 'Jesika Harries',
         age: 33,
         attendance: 70,
         gender: 'F',
         isIndian: false
     },
     {
-        name:'Json Born',
+        name: 'Json Born',
         age: 23,
         attendance: 30,
         gender: 'M',
         isIndian: false
     },
     {
-        name:'John Doe',
+        name: 'John Doe',
         age: 34,
         attendance: 70,
         gender: 'M',
@@ -70,35 +70,48 @@ var studentArray = [
 
 var logicalMenuArray = [
     {
-        url:'https://voidchetan.github.io/voidchetan/logical/broadband-plan.html',
+        url: 'https://voidchetan.github.io/voidchetan/logical/broadband-plan.html',
         text: 'Plan Selection',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
     },
     {
-        url:'https://voidchetan.github.io/voidchetan/logical/train-booking.html',
+        url: 'https://voidchetan.github.io/voidchetan/logical/train-booking.html',
         text: 'Train-Booking',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://resize.indiatvnews.com/en/resize/newbucket/1200_-/2020/05/irctc-special-trains-1589168982.jpg'
     },
     {
-        url:'https://voidchetan.github.io/voidchetan/logical/movieBooking.html',
+        url: 'https://voidchetan.github.io/voidchetan/logical/movieBooking.html',
         text: 'Movie-Booking',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://xdfile.com/wp-content/uploads/2020/01/movie-ticket-app-free-download-758x569.jpg'
-    } 
+    }
 ];
 var javascriptMenueArray = [
     {
-        url:'https://voidchetan.github.io/voidchetan//javscript/variables.html',
+        url: 'https://voidchetan.github.io/voidchetan/javscript/variables.html',
         text: 'Variables',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
-    },
+    }
+    // ,
+    // {
+    //     url:'https://voidchetan.github.io/voidchetan/javscript/variables.html',
+    //     text: 'Variables',
+    //     info: 'All Scenario on ngIf to Practice with examples',
+    //     imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
+    // },
+    // {
+    //     url:'https://voidchetan.github.io/voidchetan/javscript/variables.html',
+    //     text: 'Variables',
+    //     info: 'All Scenario on ngIf to Practice with examples',
+    //     imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
+    // },
 ];
 var languageArray = [
     "Html",
-    "CSS","Java-Script","JQuery","Angular","React","Dot Net","Java"
+    "CSS", "Java-Script", "JQuery", "Angular", "React", "Dot Net", "Java"
 ]
 
 var movieArray = [
@@ -106,7 +119,7 @@ var movieArray = [
         movieName: 'Rocketry: The Nambi Effect',
         ticketRate: '250',
         description: 'Based on the life of Indian Space Research Org scientist Nambi Narayanan, who was framed for being a spy ',
-        shows: ['09.00 AM - 12.00 AM', '12.30 PM - 1.30 PM', ' 1.30 PM - 3.30' ]
+        shows: ['09.00 AM - 12.00 AM', '12.30 PM - 1.30 PM', ' 1.30 PM - 3.30']
     },
     {
         movieName: '3 Idiots',
@@ -127,3 +140,50 @@ var movieArray = [
         shows: ['11.00 AM - 02.00 PM', '02.00 PM - 05.00 PM']
     }
 ]
+
+var index = 1;
+angularMenuArray.forEach(element => {
+    const link = " <li><a class='dropdown-item' href='" + element.url + "'>" + element.text + "</a></li>";
+    $("#angularUl ul").append(link);
+    var dynamicCard = "<div class='col-6 col-md-3 col-sm-4 col-lg-3 pt-2'>";
+    dynamicCard = dynamicCard + "<div class='card' style='padding:0px'>";
+    dynamicCard = dynamicCard + "<img class='card-img-top' src='" + element.imageUrl + "' alt='Card image'>";
+    dynamicCard = dynamicCard + "<div class='card-body'>";
+    dynamicCard = dynamicCard + "<h5 class='card-title'>" + element.text + "</h5>";
+    dynamicCard = dynamicCard + "<p class='card-text'>" + element.info + "</p>";
+    dynamicCard = dynamicCard + "<a href='" + element.url + "' class='btn btn-primary btn-sm'>See Tasks</a>";
+    dynamicCard = dynamicCard + "</div>";
+    dynamicCard = dynamicCard + "</div>";
+
+    $("#divCard").append(dynamicCard);
+});
+logicalMenuArray.forEach(element => {
+    const link = " <li><a class='dropdown-item' href='" + element.url + "'>" + element.text + "</a></li>";
+    $("#logicalUl ul").append(link);
+    var dynamicCard = "<div class='col-6 col-md-3 col-sm-4 col-lg-3 pt-2'>";
+    dynamicCard = dynamicCard + "<div class='card' style='padding:0px'>";
+    dynamicCard = dynamicCard + "<img class='card-img-top' src='" + element.imageUrl + "' alt='Card image'>";
+    dynamicCard = dynamicCard + "<div class='card-body'>";
+    dynamicCard = dynamicCard + "<h5 class='card-title'>" + element.text + "</h5>";
+    dynamicCard = dynamicCard + "<p class='card-text'>" + element.info + "</p>";
+    dynamicCard = dynamicCard + "<a href='" + element.url + "' class='btn btn-secondary btn-sm'>See Tasks</a>";
+    dynamicCard = dynamicCard + "</div>";
+    dynamicCard = dynamicCard + "</div>";
+
+    $("#divCard2").append(dynamicCard);
+});
+javascriptMenueArray.forEach(element => {
+    const link = " <li><a class='dropdown-item' href='" + element.url + "'>" + element.text + "</a></li>";
+    $("#javascriptUl ul").append(link);
+    var dynamicCard = "<div class='col-6 col-md-3 col-sm-4 col-lg-3 pt-2'>";
+    dynamicCard = dynamicCard + "<div class='card' style='padding:0px'>";
+    dynamicCard = dynamicCard + "<img class='card-img-top' src='" + element.imageUrl + "' alt='Card image'>";
+    dynamicCard = dynamicCard + "<div class='card-body'>";
+    dynamicCard = dynamicCard + "<h5 class='card-title'>" + element.text + "</h5>";
+    dynamicCard = dynamicCard + "<p class='card-text'>" + element.info + "</p>";
+    dynamicCard = dynamicCard + "<a href='" + element.url + "' class='btn btn-secondary btn-sm'>See Tasks</a>";
+    dynamicCard = dynamicCard + "</div>";
+    dynamicCard = dynamicCard + "</div>";
+
+    $("#divCardJs").append(dynamicCard);
+}); 
