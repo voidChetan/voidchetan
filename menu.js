7afb3862d35168c2f75ebf1b256d58b940089738
codefirst2022 <codefirst2022@gmail.com>
2023-04-09 15:35:11 +0530

    Array

--- menu.js
+++ menu.js
@@ -136,6 +136,12 @@ var javascriptMenueArray = [
         text: 'Object',
         info: 'how to Create Object and add new fields to it - update values fo fields',
         imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
+    }, 
+    {
+        url: 'https://voidchetan.github.io/voidchetan/javascript/array.html',
+        text: 'Object',
+        info: 'how to Create Array n array operations',
+        imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
     }
     // ,
     // {
