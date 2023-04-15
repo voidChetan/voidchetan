79894e44fdd62d4e6f3d736fad9db2eef85d2b5b
codefirst2022 <codefirst2022@gmail.com>
2023-04-15 13:06:49 +0530

    Menu

--- menu.js
+++ menu.js
@@ -88,8 +88,14 @@ var logicalMenuArray = [
         imageUrl: 'https://xdfile.com/wp-content/uploads/2020/01/movie-ticket-app-free-download-758x569.jpg'
     },
     {
-        url: 'https://voidchetan.github.io/voidchetan/logical/hotelOrder.html',
-        text: 'Hotel-Order',
+        url: 'https://voidchetan.github.io/voidchetan/logical/calculator.html',
+        text: 'calculator',
+        info: 'All Scenario on ngIf to Practice with examples',
+        imageUrl: 'https://xdfile.com/wp-content/uploads/2020/01/movie-ticket-app-free-download-758x569.jpg'
+    },
+    {
+        url: 'https://voidchetan.github.io/voidchetan/logical/zoomCar.html',
+        text: 'Zoom-Car-Rental',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://xdfile.com/wp-content/uploads/2020/01/movie-ticket-app-free-download-758x569.jpg'
     }
