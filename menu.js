fd5b34e76583a556e72380326fb43e12c0a4ea0a
codefirst2022 <codefirst2022@gmail.com>
2023-11-16 17:53:25 +0530

    re

--- menu.js
+++ menu.js
@@ -69,18 +69,7 @@ var studentArray = [
 ];
 
 var logicalMenuArray = [
-    {
-        url: 'https://voidchetan.github.io/voidchetan/logical/LogicDevelopment.html',
-        text: 'Logic Development Scenario - 1',
-        info: 'All Scenario on ngIf to Practice with examples',
-        imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
-    },
-    {
-        url: 'https://voidchetan.github.io/voidchetan/logical/LogicDevelopment2.html',
-        text: 'Logic Development Scenario - 1',
-        info: 'All Scenario on ngIf to Practice with examples',
-        imageUrl: 'https://i.pinimg.com/originals/65/70/92/657092222f678a4e905a03715f1726d8.png'
-    },
+     
     {
         url: 'https://voidchetan.github.io/voidchetan/logical/broadband-plan.html',
         text: 'Plan Selection',
