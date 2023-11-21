d5b62c23f6dfd3154f4c68590aba8a1a982b36e0
codefirst2022 <codefirst2022@gmail.com>
2023-11-21 21:29:08 +0530

    Movie

--- menu.js
+++ menu.js
@@ -123,7 +123,14 @@ var logicalMenuArray = [
         text: 'Student-Registration',
         info: 'All Scenario on ngIf to Practice with examples',
         imageUrl: 'https://xdfile.com/wp-content/uploads/2020/01/movie-ticket-app-free-download-758x569.jpg'
+    }, 
+    {
+        url: 'https://voidchetan.github.io/voidchetan/logical/movie-seat-booking.html',
+        text: 'Movie Seat Booking',
+        info: 'All Scenario on ngIf to Practice with examples',
+        imageUrl: 'https://xdfile.com/wp-content/uploads/2020/01/movie-ticket-app-free-download-758x569.jpg'
     }
+
 ];
 var logicalDevelopmentMenuArray = [
     {
